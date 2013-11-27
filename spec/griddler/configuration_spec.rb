e1cb63a4b6c28e8887028313e8a286b27e6d146c
Caleb Thompson <caleb@calebthompson.io>
2013-11-27 11:02:55 -0500

    Make sure that Griddler is configured
    Fixes intermittent failure.

--- spec/griddler/configuration_spec.rb
+++ spec/griddler/configuration_spec.rb
@@ -13,12 +13,12 @@ describe Griddler::Configuration do
       Griddler.configuration.reply_delimiter.should eq('Reply ABOVE THIS LINE')
       Griddler.configuration.email_service.should eq(Griddler::Adapters::SendgridAdapter)
     end
-  end
 
-  it 'raises a helpful error if EmailProcessor is undefined' do
-    Kernel.stub(const_defined?: false)
+    it 'raises a helpful error if EmailProcessor is undefined' do
+      Kernel.stub(const_defined?: false)
 
-    expect { Griddler.configuration.processor_class }.to raise_error(NameError, %r{https://github\.com/thoughtbot/griddler#defaults})
+      expect { Griddler.configuration.processor_class }.to raise_error(NameError, %r{https://github\.com/thoughtbot/griddler#defaults})
+    end
   end
 
   describe 'with config block' do
